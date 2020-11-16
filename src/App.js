c69a5336af145d0c8e92ba03d4cfe1000edb47c8
Santiago <santo1996.29@gmail.com>
2020-11-16 17:17:36 -0400

    feature: Router and some components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Projects from './components/projects/Projects'
 
 function App() {
-    return <h1>App component</h1>
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/projects" component={Projects} />
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <h1>Projects component</h1>
+    )
+}
+
+export default Projects
