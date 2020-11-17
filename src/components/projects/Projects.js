0a1dadccaf5e5bc08550c4a75f27ac7291d45d25
Santiago <santo1996.29@gmail.com>
2020-11-17 11:12:39 -0400

    feature: Projects jsx and Sidebar component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Sidebar from '../layout/Sidebar'
 
 const Projects = () => {
-    return <h1>Projects component</h1>
+    return (
+        <div className="contenedor-app">
+            <Sidebar />
+            <div className="contenedor-principal">
+                <main>
+                    <div className="contenedor-tareas">
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
 }
 
 export default Projects
