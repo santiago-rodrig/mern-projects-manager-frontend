ac20f172681e19ef6f2b09cc999f06db1f476a6a
Santiago <santo1996.29@gmail.com>
2020-11-17 11:31:18 -0400

    feature: state for ProjectForm component

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,17 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const ProjectForm = () => {
+    const [project, setPoject] = useState({
+        name: '',
+    })
+
+    const { name } = project
+
+    const handleChange = (e) => {
+        setPoject({
+            ...project,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <Fragment>
             <button type="button" className="btn btn-block btn-primario">
                 Nuevo Proyecto
             </button>
-            <form className="formulario-nuevo-proyecto">
+            <form className="formulario-nuevo-proyecto" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     className="input-text"
                     placeholder="Climb the Everest"
                     name="name"
+                    value={name}
+                    onChange={handleChange}
                 />
                 <input
                     type="submit"
