c87bda5bd5b5901ab806892ffd70b06b77563cd7
Santiago <santo1996.29@gmail.com>
2020-11-17 12:54:20 -0400

    feature: MainBar component for Projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from '../layout/Sidebar'
+import MainBar from '../layout/MainBar'
 
 const Projects = () => {
     return (
         <div className="contenedor-app">
             <Sidebar />
             <div className="contenedor-principal">
+                <MainBar />
                 <main>
                     <div className="contenedor-tareas"></div>
                 </main>
