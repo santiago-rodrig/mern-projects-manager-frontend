6708f86e01fef924b07b4fd6654e659412178dbc
Santiago Rodriguez <santo1996.29@gmail.com>
2020-11-29 21:45:32 -0400

    alerts context

---
+++ src/contexts/alerts.js
@@ -0,0 +1,19 @@
+import { createContext, useReducer } from 'react'
+import alertsReducer, { SHOW_ALERT, HIDE_ALERT } from '../reducers/alerts'
+export const alertsContext = createContext()
+export default AlertsContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(alertsReducer, {
+        alert: null,
+    })
+    const showAlert = (msg, category) => {
+        dispatch({ type: SHOW_ALERT, payload: { msg, category } })
+        window.setTimeout(() => {
+            dispatch({ type: HIDE_ALERT })
+        }, 5000)
+    }
+    return (
+        <alertsContext.Provider value={{ alert: state.alert, showAlert }}>
+            {children}
+        </alertsContext.Provider>
+    )
+}
---
+++ src/reducers/alerts.js
@@ -0,0 +1,19 @@
+export const SHOW_ALERT = 'SHOW_ALERT'
+export const HIDE_ALERT = 'HIDE_ALERT'
+export default (state, action) => {
+    const { type, payload } = action
+    switch (type) {
+        case SHOW_ALERT:
+            return {
+                ...state,
+                alert: payload,
+            }
+        case HIDE_ALERT:
+            return {
+                ...state,
+                alert: null,
+            }
+        default:
+            return state
+    }
+}
